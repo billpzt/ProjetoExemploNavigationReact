3a2f2f249f0009072ac80cb85a46c80737976ba1
Billpzt <billpiazzetta@gmail.com>
2023-05-04 11:53:24 -0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackActions } from '@react-navigation/native'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from './views/Home'
+import About from './views/About'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name='Home' 
+            component={Home}
+          />
+          <Stack.Screen 
+            name='About' 
+            component={About}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
